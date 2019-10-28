a4b13db568555864d1428eb3b8e9b739d77be765
Albert Malmkvist <Malmkvist.albert@gmail.com>
2019-10-28 09:33:19 +0100

    Everything in for now

---
+++ Backgroundscript.js
@@ -0,0 +1,267 @@
+var x = 0;
+
+var StartButton = document.querySelector(".Change");
+StartButton.addEventListener("click", function(){ Switch()})
+
+function Switch() {
+    if (x==0){
+        X++
+
+ var FN = document.createElement("label");
+ Fn.setAttribute("id","Fn");
+Fn.text = "First Name";
+ var FNI = document.createElement("input");
+ Race.setAttribute("id","");
+
+ var LN = document.createElement("label");
+ Race.setAttribute("id","");
+
+ var LNI = document.createElement("input");
+ Race.setAttribute("id","");
+
+ var RaceL = document.createElement("label");;
+ Race.setAttribute("id","RaceL");
+
+var Race = document.createElement("select");
+Race.setAttribute("id","Race");
+
+ var RacDra = document.createElement("option");
+Racdra.setAttribute("value","Dragonborn");
+ RacDra.text = "Dragonborn";
+ var Racdwa = document.createElement("option");
+ Racdwa.setAttribute("value","Dwarf");
+   Racdwa.text="Dwarf"
+ var Rac = document.createElement("option");
+ Racelf.setAttribute("value","Elf");
+   Racelf.text="Elf"
+ var Racelf = document.createElement("option");
+ Racorc.setAttribute("value","Gnome");
+   Racgno.text="Gnome"
+ var Rachelf = document.createElement("option");
+ RacHelf.setAttribute("value","Half-elf");
+   RacHelf.text="Halfelf"
+ var Rachorc = document.createElement("option");
+ RacHorc.setAttribute("value","Halforc");
+   RacHorc.text="Halforc"
+ var RacHorc = document.createElement("option");
+ Rachal.setAttribute("value","Halfling");
+   RacHal="Halfling"
+ var RacHum = document.createElement("option");
+ RacHum.setAttribute("value","Human");
+   RacHum.text="Human"
+ var Ractie = document.createElement("option");
+ Ractie.setAttribute("value","Tiefling");
+   Ractie.text ="Tiefling"
+
+   Race.add(Racelf);
+   Race.add(RacDra);
+   Race.add(RacHelf);
+   Race.add(RacHorc);
+   Race.add(RacHum);
+   Race.add(Racdwa);
+   Race.add(Racgno);
+   Race.add(Ractie);
+
+
+
+
+var one = document.createElement("select");
+var one = document.createElement("option");
+// label for="Al"Alignment/label
+ var one = document.createElement("radio");
+// input type="radio" name="Alignment"var  value="Good Lawful"  Good Lawful
+var one = document.createElement("radio");
+//input type="radio" name="Alignment"var  value="Good Neutral" Good Neutral
+var one = document.createElement("radio");
+//input type="radio" name="Alignment"var  value="Good Chaotic" Good Chaotic 
+var one = document.createElement("radio");
+//input type="radio" name="Alignment"var  value="Neutral Lawful"  Neutral Lawful
+var one = document.createElement("radio");
+//input type="radio" name="Alignment"var  value="Neutral" checked Neutral
+var one = document.createElement("radio");
+//input type="radio" name="Alignment"var  value="Neutral Chaotic" Neutral Chaotic
+var one = document.createElement("radio");
+//input type="radio" name="Alignment"var  value="Evil Lawful"  Evil Lawful
+var one = document.createElement("radio");
+//input type="radio" name="Alignment"var  value="Evil Neutral" Evil Neutral
+var one = document.createElement("radio");
+//input type="radio" name="Alignment"var  value="Evil Chaotic" Evil Chaotic 
+var one = document.createElement("Label");
+//label for="C"Class/label
+var one = document.createElement("select");
+//select name="Class" id="Class"
+var one = document.createElement("option");
+Race.setAttribute("value","");
+   var  value="Fighter"
+   var one = document.createElement("option");
+Race.setAttribute("value","");
+   var  value="Wizard"
+   var one = document.createElement("option");
+Race.setAttribute("value","");
+   var  value="Sorcerrer"
+   var one = document.createElement("option");
+Race.setAttribute("value","");
+   var  value="Bard"
+   var one = document.createElement("option");
+Race.setAttribute("value","");
+   var  value="Warlock"
+   var one = document.createElement("option");
+Race.setAttribute("value","");
+   var  value="Cleric"
+   var one = document.createElement("option");
+Race.setAttribute("value","");
+   var  value="Paladin"
+   var one = document.createElement("option");
+Race.setAttribute("value","");
+   var  value="Rouge"
+   var one = document.createElement("option");
+Race.setAttribute("value","");
+   var  value="Druid"
+   var one = document.createElement("option");
+Race.setAttribute("value","");
+   var  value="Monk"
+   var one = document.createElement("option");
+Race.setAttribute("value","");
+   var  value="Ranger"
+   var one = document.createElement("option");
+Race.setAttribute("value","");
+   var  value="Barbarian"
+   var one = document.createElement("option");
+Race.setAttribute("value","");
+
+var one = document.createElement("label");
+//label for="Back"Background/label
+var one = document.createElement("select");
+//select name="Background" id="Background"
+var one = document.createElement("option");
+Race.setAttribute("value","");
+   var  value="Criminal"
+   var one = document.createElement("option");
+Race.setAttribute("value","");
+   var  value="Acolyte"
+   var one = document.createElement("option");
+Race.setAttribute("value","");
+   var  value="Orphan"
+   var one = document.createElement("option");
+Race.setAttribute("value","");
+   var  value="Soldier"
+   var one = document.createElement("option");
+Race.setAttribute("value","");
+   var  value="Sage"
+   var one = document.createElement("option");
+Race.setAttribute("value","");
+   var  value="Hermit"
+   var one = document.createElement("option");
+Race.setAttribute("value","");
+   var  value="Noble"
+   var one = document.createElement("option");
+Race.setAttribute("value","");
+   var  value="Sailor"
+   var one = document.createElement("option");
+Race.setAttribute("value","");
+   var  value="Entertainer"
+   var one = document.createElement("option");
+Race.setAttribute("value","");
+   var  value="Folk Hero" 
+   var one = document.createElement("option");
+Race.setAttribute("value","");
+   var  value="Charlatan"
+   var one = document.createElement("option");
+Race.setAttribute("value","");
+   var  value="Guild Artisan" 
+   var one = document.createElement("option");
+Race.setAttribute("value","");
+   var  value="Outlander"
+   var one = document.createElement("option");
+Race.setAttribute("value","");
+
+var one = document.createElement("label");
+//label for="str"Strenght/label
+var one = document.createElement("select");
+//select name="Strenght" id="Strenght"
+ var one = document.createElement("option");
+Race.setAttribute("value","1");
+ var one = document.createElement("option");
+Race.setAttribute("value","2");
+ var one = document.createElement("option");
+Race.setAttribute("value","3");
+ var one = document.createElement("option");
+Race.setAttribute("value","4");
+ var one = document.createElement("option");
+Race.setAttribute("value","5");
+ var one = document.createElement("option");
+Race.setAttribute("value","6");
+ var one = document.createElement("option");
+Race.setAttribute("value","7");
+ var one = document.createElement("option");
+Race.setAttribute("value","8");
+ var one = document.createElement("option");
+Race.setAttribute("value","9");
+ var one = document.createElement("option");
+Race.setAttribute("value","10");
+ var one = document.createElement("option");
+Race.setAttribute("value","11");
+ var one = document.createElement("option");
+Race.setAttribute("value","12");
+ var one = document.createElement("option");
+Race.setAttribute("value","13");
+ var one = document.createElement("option");
+Race.setAttribute("value","14");
+ var one = document.createElement("option");
+Race.setAttribute("value","15");
+ var one = document.createElement("option");
+Race.setAttribute("value","16");
+ var one = document.createElement("option");
+Race.setAttribute("value","17");
+ var one = document.createElement("option");
+Race.setAttribute("value","18");
+ var one = document.createElement("option");
+Race.setAttribute("value","19");
+ var one = document.createElement("option");
+Race.setAttribute("value","20");
+//label for="dex"Dexterity/label
+var one = document.createElement("label");
+//select name="Dexterity" id="Dexterity"
+var one = document.createElement("select");
+//label for="con"Constitution/label
+var one = document.createElement("label");
+//select name="Constitution" id="Constitution"
+var one = document.createElement("select");
+//label for="int"Intelligence/label
+var one = document.createElement("label");
+//select name="Intelligence" id="Intelligence"
+var one = document.createElement("select");
+//label for="wis"Wisdom/label
+var one = document.createElement("label");
+//select name="Wisdom" id="Wisdom"
+var one = document.createElement("select");
+//label for="cha"Charisma/label
+var one = document.createElement("label");
+//select name="Charisma" id="Charisma"
+var one = document.createElement("select");
+//button id="P1" onclick="Switch();"Change/button
+
+
+    }
+    else if(x == 1) {
+var SRace = document.createElement("select");
+SRace.setAttribute("id", "Subrace");
+
+var SClass = document.createElement("SubClass");
+        x++;
+    }
+    else if (x == 2) {
+        x++
+    } 
+    else if (x == 3) {
+var Race = document.createElement("Race");
+
+
+var Class = document.createElement("Class");
+        
+        x = 1;
+    }
+    else {
+        document.write("Something oke");
+    }
+}
---
+++ Backgroundscript2.js
@@ -0,0 +1,93 @@
+var x = 1;
+var Label1 = document.getElementsByName("Label1");
+var Label2 = document.getElementsByName("Label2");
+var PAlingement = document.getElementsByName("PAlingement");
+for (let i = 0, length = Label2.length; i < length; i++) {
+    Label2[i].style.visibility = "hidden";
+}
+function Switch() {
+    if (x == 1) {
+        Steep1();
+        x = 2;
+    } else if (x == 2) {
+        Steep2();
+        x = 3;
+    } else if (x == 3) {
+        Steep3();
+        x = 1;
+    } else {
+        document.write("Something is wrong");
+    }
+}
+function Steep1() {
+    for (let i = 0, length = Label1.length; i < length; i++) {
+        Label1[i].style.visibility = "hidden";
+    }
+    for (let i = 0, length = PAlingement.length; i < length; i++) {
+        PAlingement[i].style.visibility = "hidden";
+    }
+    for (let i = 0, length = Label2.length; i < length; i++) {
+        Label2[i].style.visibility = "visible";
+    }
+    var SubClass = document.getElementByid("SubClass"); var Domain = document.getElementByid("Domain"); var Origin = document.getElementByid("Origin"); var Patron = document.getElementByid("Patron"); var Fight = document.getElementByid("Fight");
+    var SubRace = document.getElementByid("SubRace"); var Dwarf = document.getElementByid("SubDwarf"); var Elf = document.getElementByid("SubElf"); var Dragon = document.getElementByid("SubDragon"); var Gnome = document.getElementByid("SubGnome");
+
+    var Class = document.getElementByid("Class").value;
+    if (ChClass = "Cleric") {
+        SubClass.innerHTML = "Domain";
+        Patron.style.visibility = "hidden";
+        origin.style.visibility = "hidden";
+        Fight.style.visibility = "hidden";
+    }
+    else if (ChClass = "Warlock") {
+        SubClass.innerHTML = "Patron";
+        Fight.style.visibility = "hidden";
+        origin.style.visibility = "hidden";
+        Domain.style.visibility = "hidden";
+    }
+    else if (ChClass = "Sorcerrer") {
+        SubClass.innerHTML = "Sorcerous Origin";
+        Domain.style.visibility = "hidden";
+        Fight.style.visibility = "hidden";
+        Patron.style.visibility = "hidden";
+    }
+    else if (ChClass = "Fighter") {
+        SubClass.innerHTML = "Fighting Style";
+        Domain.style.visibility = "hidden";
+        Patron.style.visibility = "hidden";
+        origin.style.visibility = "hidden";
+    } else {
+        origin.style.visibility = "hidden";
+        Domain.style.visibility = "hidden";
+        Fight.style.visibility = "hidden";
+        Patron.style.visibility = "hidden";
+        SubClass.style.visibility = "hidden";
+    }
+
+}
+function Steep2() {
+    for (let i = 0, length = Label2.length; i < length; i++) {
+        Label2[i].style.visibility = "hidden";
+    }
+    //var FN = document.getElementByid("FN").value; var LN = document.getElementByid("LN").value;
+    //var Race = document.getElementByid("Race").value; var SRace = document.getElementByid("SRace").value;
+    //var Alignment = document.getElementsByName("Alignment").value;
+
+    //var SubClass = document.getElementByid("SubClass").value; var Domain = document.getElementByid("Domain").value; var Origin = document.getElementByid("Origin").value; var Patron = document.getElementByid("Patron").value; var Fight = document.getElementByid("Fight").value;
+    //var Back = document.getElementByid("Back").value; var Background = document.getElementByid("Background").value;
+
+    //var Strenght = document.getElementByid("Strenght").value;
+    //var Dexterity = document.getElementByid("Dexterity").value;
+    //var Constitution = document.getElementByid("Constitution").value;
+    //var intelligence = document.getElementByid("intelligence").value;
+    //var Wisdom = document.getElementByid("Wisdom").value;
+    //var Charisma = document.getElementByid("Charisma").value;
+
+    //document.getElementByid("Conclusion").innerHTML = "Your name is "+FN+" "+LN+".<br> Your Class is";
+}
+function Steep3() {
+    for (let i = 0, length = Label1.length; i < length; i++) {
+        Label1[i].style.visibility = "visible";
+    }
+    //document.getElementByid("Conclusion").innerHTML = "";
+}
